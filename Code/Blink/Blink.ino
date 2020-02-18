a0f1f82b15803dd3afd485044810637cdece06cf
hbr890627 <hbr890627@gmail.com>
2020-02-18 22:59:00 +0800

    Update Blink.ino

--- Code/Blink/Blink.ino
+++ Code/Blink/Blink.ino
@@ -6,15 +6,13 @@
 我是註解
 */
 
-
-
 void setup() {
   pinMode(13, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(1000);
 }
